fd478a11e1527630abee0d265989271e2a97811a
Joern Klinger <ungewiss@gmail.com>
2015-09-16 20:34:34 +0200

    Functions: initialize model, toy data

--- classifier.py
+++ classifier.py
@@ -14,6 +14,28 @@ def logsumexp(a):
 # I though maybe the real data was too noisy,
 # but we face the same problems when using toy data
 
+
+def init_parameters():
+    ''' Quickly initializes core parameters '''
+    global groups
+    global users
+    global features
+    global dividing_line
+    global group_probabilities_log
+    global conditional_probabilities_log
+    global probability_user_in_group_log
+    global last_probability_data_given_parameters_log
+
+    groups = 3
+    users = 12
+    features = 6
+    dividing_line = 0.8
+    group_probabilities_log = None
+    conditional_probabilities_log = None
+    probability_user_in_group_log = None
+    last_probability_data_given_parameters_log = None
+
+
 def create_toy_data(groups, users, features, dividing_line):
     ''' Creates toy data set with 2 classes. With nr of users = users and nr of features = features '''
     if np.mod(users, groups) != 0:
@@ -35,14 +57,29 @@ def create_toy_data(groups, users, features, dividing_line):
 
     return toy_data
 
+def initialize_model():
+    ''' Initialize the naive bayes model '''
+    global group_probabilities_log
+    global conditional_probabilities_log
+    global probability_user_in_group_log
+    global last_probability_data_given_parameters_log
+    # probabilities that a randomly drawn user is in class 0...n
+    group_probabilities_log = np.empty(groups)
+    group_probabilities_log.fill(np.log(1.0/groups))
 
+    # conditional probabilities for each feature by group
+    conditional_probabilities_log = np.empty([features, groups], dtype=float)
 
+    # for one class we initialize them all as log(0.5)
+    # for the other class we initialize them as a random close to log(0.5)
+    conditional_probabilities_log[:, 0].fill(log(1.0/groups))
 
+    mu, sigma = 0.5, 0.0001
+    conditional_probabilities_log[:, 1:groups] = (np.log(np.random.normal(mu, sigma, [features, groups-1])))
 
+    # probability_user_in_group_log rows: users, cols: log([p(class0), p(class1)])
+    probability_user_in_group_log = np.empty([users, groups], dtype=float)
 
+    # Initialize last_probability_data_given_parameters_log as negative infinity
+    last_probability_data_given_parameters_log = -np.inf
 
-
-
-    return toy_data
-
-data = create_toy_data()
