9b11cecd360e85d434d964c7565363054be88f65
Joern Klinger <ungewiss@gmail.com>
2015-09-17 20:00:22 +0200

    Run script to test the classifier.

--- script.py
+++ script.py
@@ -1,5 +1,3 @@
-# See http://nbviewer.ipython.org/gist/joernklinger/e4c4936ad09bc1560546
-
 from classifier import *
 
 # Initialize model
@@ -9,7 +7,6 @@ model = initialize_model(model=model)
 # Create toy data
 data = create_toy_data(model=model, dividing_line=0.8)
 
-# Perform step1
-model = step1(model, data)
-
-
+# Run model for some iterations
+for i in xrange(50):
+    model = iteration(model, data)
