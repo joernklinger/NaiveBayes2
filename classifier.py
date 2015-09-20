87cbee8bf3222020b3c05f4df59f298f0d87be4a
Joern Klinger <ungewiss@gmail.com>
2015-09-20 21:04:48 +0200

    saves model files, finds best model

--- classifier.py
+++ classifier.py
@@ -1,6 +1,10 @@
+# See http://nbviewer.ipython.org/gist/joernklinger/e4c4936ad09bc1560546
+
 # Import modules
 import numpy as np
 import pdb
+from datetime import datetime
+import os
 
 # Dict with .attributes
 class dotdict:
@@ -12,7 +16,8 @@ def initialize_model(model, groups=3, users=12, features=6):
     model.groups = groups
     model.users = users
     model.features = features
-    # probabilities that a randomly drawn user is in class 0...n
+
+    # Probabilities that a randomly drawn user is in class 0...n
     model.group_probabilities_log = np.empty(model.groups)
     model.group_probabilities_log.fill(np.log(1.0/model.groups))
 
@@ -23,16 +28,20 @@ def initialize_model(model, groups=3, users=12, features=6):
     # for the other class we initialize them as a random close to log(0.5)
     model.conditional_probabilities_log[:, 0].fill(np.log(1.0/model.groups))
 
-    mu, sigma = 0.5, 0.0001
+    mu, sigma = 1.0/model.groups, 0.0001
     model.conditional_probabilities_log[:, 1:model.groups] = (np.log(np.random.normal(mu, sigma, [model.features, model.groups-1])))
     model.conditional_probabilities_log = np.transpose(model.conditional_probabilities_log)
 
-    # probability_is_1_user_in_group_log rows: users, cols: log([p(class0), p(class1)])
-    model.probability_is_1_user_in_group_log = np.empty([model.users, model.groups], dtype=float)
+    # probability_user_in_group_log rows: users, cols: log([p(class0), p(class1)])
+    model.probability_user_in_group_log = np.empty([model.users, model.groups], dtype=float)
 
-    # Initialize last_probability_is_1_data_given_parameters_log as negative infinity
-    model.probability_is_1_data_given_parameters_log = None
-    model.last_probability_is_1_data_given_parameters_log = -np.inf
+    # Initialize last_probability_data_given_parameters_log as negative infinity
+    model.probability_data_given_parameters_log = None
+    model.last_probability_data_given_parameters_log = -np.inf
+
+    # Model Status
+    model.status = None
+
     return model
 
 
@@ -58,26 +67,26 @@ def create_toy_data(model, probability_is_1):
 
 
 def step1(model, data):
-    ''' calculate probability_is_1 of each user being in group 0...n '''
-    if(model.probability_is_1_data_given_parameters_log != None):
-        model.last_probability_is_1_data_given_parameters_log = model.probability_is_1_data_given_parameters_log
+    ''' calculate probability of each user being in group 0...n '''
+    if(model.probability_data_given_parameters_log != None):
+        model.last_probability_data_given_parameters_log = model.probability_data_given_parameters_log
 
     for row in xrange(model.users):
-        model.probability_is_1_user_in_group_log[row] = (data[row,]*model.conditional_probabilities_log).sum(axis=1) + model.group_probabilities_log
-        # Normalize probability_is_1_user_in_class
-        model.probability_is_1_user_in_group_log[row] = model.probability_is_1_user_in_group_log[row]-logsumexp(model.probability_is_1_user_in_group_log[row,])
+        model.probability_user_in_group_log[row] = (data[row,]*model.conditional_probabilities_log).sum(axis=1) + model.group_probabilities_log
+        # Normalize probability_user_in_class
+        model.probability_user_in_group_log[row] = model.probability_user_in_group_log[row]-logsumexp(model.probability_user_in_group_log[row,])
     return model
 
 
 def step2(model, data):
-    ''' use probability_is_1_user_in_class_log and the actual data to update the conditional_probabilities_log '''
+    ''' use probability_user_in_class_log and the actual data to update the conditional_probabilities_log '''
     feature_counts = np.ones([model.features, model.groups], dtype=float)
     for col in xrange(model.features):
         z_norm = np.zeros(model.groups)
         for row in xrange(model.users):
             if data[row, col] == 1:
-                feature_counts[col] += np.exp(model.probability_is_1_user_in_group_log[row])
-            z_norm += np.exp(model.probability_is_1_user_in_group_log[row])
+                feature_counts[col] += np.exp(model.probability_user_in_group_log[row])
+            z_norm += np.exp(model.probability_user_in_group_log[row])
         model.conditional_probabilities_log[:,col] = np.log(feature_counts[col]) - np.log(z_norm)
     return model
 
@@ -85,35 +94,41 @@ def step2(model, data):
 def step3(model):
     ''' Update group probailities log '''
     for group in xrange(model.groups):
-        model.group_probabilities_log[group] = logsumexp(model.probability_is_1_user_in_group_log[:,group])-np.log(model.users)
+        model.group_probabilities_log[group] = logsumexp(model.probability_user_in_group_log[:,group])-np.log(model.users)
     return model
 
 
 def step4(model, data):
-    ''' Calculate probability_is_1 of the entire data given current parameters '''
-    model.probability_is_1_user_given_data_log = np.zeros(model.users, dtype=float)
+    ''' Calculate probability of the entire data given current parameters '''
+    model.probability_user_given_data_log = np.zeros(model.users, dtype=float)
     for user in xrange(model.users):
         for feature in xrange(model.features):
             if data[user][feature] == 1:
                 p_user_given_group_log = np.empty(model.groups)
                 p_user_given_group_log = model.conditional_probabilities_log[:,feature] + p_user_given_group_log
-                model.probability_is_1_user_given_data_log[user] += logsumexp(p_user_given_group_log)
-    model.probability_is_1_data_given_parameters_log = np.sum(model.probability_is_1_user_given_data_log)
+                model.probability_user_given_data_log[user] += logsumexp(p_user_given_group_log)
+    model.probability_data_given_parameters_log = np.sum(model.probability_user_given_data_log)
     return model
 
 
 def get_model_stats(model):
     ''' get model stastistics '''
     keep_going = 1
-    print 'Log Likelihood: ' + str(model.probability_is_1_data_given_parameters_log)
+    status = 'Running'
+    print 'Log Likelihood: ' + str(model.probability_data_given_parameters_log)
     print 'Raw Class Probabilities: ' + str(np.exp(model.group_probabilities_log))
-    if (model.probability_is_1_data_given_parameters_log < model.last_probability_is_1_data_given_parameters_log):
+    if (model.probability_data_given_parameters_log < model.last_probability_data_given_parameters_log):
         print 'Warning, loglikelihood decreases. LOCAL MINIMUM.'
         keep_going = 0
-    return keep_going
+        status = 'Local Minimum'
+    elif (round(model.probability_data_given_parameters_log,3) == round(model.last_probability_data_given_parameters_log,3)):
+        print 'Model converged.'
+        keep_going = 0
+        status = 'Converged'
+    return keep_going, status
 
 
-def iteration(model, data):
+def iterate_model(model, data):
     # Perform step1
     model = step1(model=model, data=data)
     # Perform step2
@@ -122,9 +137,36 @@ def iteration(model, data):
     model = step3(model=model)
     # Perform step4
     model = step4(model=model, data=data)
-    # Get stats
-    get_model_stats(model)
     return model
+
+
+def run_models(models_to_run, attempts_at_each_model, max_iterations, data):
+    results_report = []
+    for model in models_to_run:
+        save_dir_name = 'groups_' + str(model.groups) + '_users_' + str(model.users) + '_features_' + str(model.features) + '_time_' + str(datetime.now()).replace(' ', '_')
+        os.makedirs('results/' + save_dir_name)
+        for attempt in xrange(attempts_at_each_model):
+            for iteration in xrange(max_iterations):
+                model = iterate_model(model, data)
+                keep_going, status = get_model_stats(model)
+                if (keep_going == 0):
+                    model.status = status
+                    save_file_name = 'model_' + str(attempt+1) + '_iterations_' + str(iteration+1) + '_groups_' + str(model.groups) + '_users_' + str(model.users) + '_features_' + str(model.features) + '_model_nr_' + str(attempt) + '_time_' + str(datetime.now()).replace(' ', '_')
+                    np.save('results/' + save_dir_name + '/' + save_file_name, model)
+                    results_report.append([model.last_probability_data_given_parameters_log, save_dir_name, save_file_name])
+                    break
+    results_report = np.asarray(results_report)
+    report_file_name = 'report_' + str(datetime.now()).replace(' ', '_')
+    np.save('results/' + report_file_name, results_report)
+    return results_report
+
+
+# Load best model
+def get_best_model(results_report):
+    best_model_index = np.argmax(results_report[:,0])
+    best_model_file_name ='results/' +  results_report[best_model_index][1] + '/' + results_report[best_model_index][2] + '.npy'
+    best_model = np.load(best_model_file_name)[()]
+    return best_model
 
 
 def logsumexp(a):
--- script.py
+++ script.py
@@ -2,11 +2,22 @@ from classifier import *
 
 # Initialize model
 model = dotdict()
-model = initialize_model(model=model)
+model = initialize_model(model, groups=3, users=120, features=30)
 
-# Create toy data
-data = create_toy_data(model=model, probability_is_1=0.8)
+# Initialize model2
+model2 = dotdict()
+model2 = initialize_model(model2, groups=5, users=120, features=30)
 
-# Run model for some iterations
-for i in xrange(50):
-    model = iteration(model, data)
+# Create toy data (based on model)
+data = create_toy_data(model=model, probability_is_1=0.66)
+
+# Add models to models_to_run arrayy
+models_to_run=[]
+models_to_run.append(model)
+models_to_run.append(model2)
+
+# Run models
+results_report = run_models(models_to_run, attempts_at_each_model=5, max_iterations=5, data=data)
+
+# Get best model
+best_model = ger_best_model(results_report)
